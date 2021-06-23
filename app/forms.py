884a8e11febf2c5b774b7227e769979bf1b5c671
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-06-23 13:09:24 +0300

    Add username validation on edit_profile form

--- app/forms.py
+++ app/forms.py
@@ -33,3 +33,13 @@ class EditProfileForm(FlaskForm):
     username = StringField(label="Username", validators=[DataRequired()])
     about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_name = original_name
+
+    def validate_username(self, username):
+        if username.data != self.original_name:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different name.')
