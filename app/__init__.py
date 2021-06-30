4085fa6a76a3b2e20b14d9ccebd2643776249a5a
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-06-30 17:27:03 +0300

    Show timestamp in user's time

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,6 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
