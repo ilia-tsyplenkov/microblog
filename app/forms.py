9fdc2d1a1e7205800841cdaf9acb9ae782050625
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-07-01 19:09:48 +0300

    Increase localization

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
-from flask_babel import Babel, lazy_gettext as _l
+from flask_babel import Babel, lazy_gettext as _l, _
 
 from app.models import User
 
@@ -16,25 +16,25 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("Please use a different name.")
+            raise ValidationError(_("Please use a different name."))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError("Please use a different email address.")
+            raise ValidationError(_("Please use a different email address."))
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label="Username", validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    remember_me = BooleanField("Remember Me")
-    submit = SubmitField('Sign In')
+    username = StringField(label=_l("Username"), validators=[DataRequired()])
+    password = PasswordField(label=_l('Password'), validators=[DataRequired()])
+    remember_me = BooleanField(_l("Remember Me"))
+    submit = SubmitField(_l('Sign In'))
 
 
 class EditProfileForm(FlaskForm):
-    username = StringField(label="Username", validators=[DataRequired()])
-    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
+    username = StringField(label=_l("Username"), validators=[DataRequired()])
+    about_me = TextAreaField(_l("About me"), validators=[Length(min=0, max=140)])
+    submit = SubmitField(_l('Submit'))
 
     def __init__(self, original_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -44,19 +44,19 @@ class EditProfileForm(FlaskForm):
         if username.data != self.original_name:
             user = User.query.filter_by(username=username.data).first()
             if user is not None:
-                raise ValidationError('Please use a different name.')
+                raise ValidationError(_('Please use a different name.'))
 
 class PostForm(FlaskForm):
-    post = TextAreaField(label='Say something', validators=[DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField('Submit')
+    post = TextAreaField(label=_l('Say something'), validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField(_l('Submit'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField(label='email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Reset Password Request')
+    email = StringField(label=_l('Email'), validators=[DataRequired(), Email()])
+    submit = SubmitField(_l('Reset Password Request'))
 
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField(label="Password", validators=[DataRequired()])
-    password2 = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
+    password = PasswordField(label=_l("Password"), validators=[DataRequired()])
+    password2 = PasswordField(label=_l("Confirm Password"), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_l('Request Password Reset'))
