71ff77f17dc2e94811d75631d0ff97357917fb59
Ilya Tsyplenkov <ilya@mindsdb.com>
2021-07-12 10:42:20 +0300

    Done searching for posts

--- app/main/routes.py
+++ app/main/routes.py
@@ -26,6 +26,7 @@ def before_request():
     g.locale = str(get_locale())
 
 
+
 @bp.route("/", methods=["GET", "POST"])
 @bp.route("/index", methods=["GET", "POST"])
 @login_required
@@ -121,3 +122,18 @@ def translate_text():
     return jsonify({'text': translate(request.form['text'],
                                       request.form['source_language'],
                                       request.form['dest_language'])})
+
+
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page, current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) if page > 1 else None
+
+    return render_template('search.html', title=_('Search'),
+                posts=posts, prev_url=prev_url, next_url=next_url)
